188b7de1a38e08ab5cc7da05295bbdcf89ecf098
shakmatton <shakmatton@hotmail.com>
2026-07-23 13:23:34 -0300

    recreated flashlight & improved scene

---
+++ Assets/Scripts/Flashlight.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class Flashlight : MonoBehaviour
+{
+    // public Transform rightHand;
+    //
+    // public Vector3 localPosition;
+    // public Vector3 localRotation;
+    //
+    // private void Start()
+    // {
+    //     transform.SetParent(rightHand, false);
+    //
+    //     transform.localPosition = Vector3.zero;
+    //     transform.localRotation = Quaternion.identity;
+    //     
+    //     // transform.localPosition = localPosition;
+    //     // transform.localRotation = Quaternion.Euler(localRotation);
+    // }
+}
