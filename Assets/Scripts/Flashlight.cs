bd242aee170c65a1460aa8d7f4b7a2d17427f371
shakmatton <shakmatton@hotmail.com>
2026-07-28 12:57:22 -0300

    added raycasting for flashlight

--- Assets/Scripts/Flashlight.cs
+++ Assets/Scripts/Flashlight.cs
@@ -11,14 +11,50 @@ namespace Scripts
 {
     public class Flashlight : MonoBehaviour
     {
-        public LayerMask myLayer;
+        [SerializeField] private LayerMask myLayer;
+        private Ray ray;
+        private float maxDistance = 5f; 
 
-        private void Start()
+        private void Update()
         {
-            Ray ray = new Ray(transform.position, transform.forward);
-            Physics.Raycast(ray, out RaycastHit hit, 1000, myLayer);
-            //hit.collider.gameObject.     // continuar...
+            ray = new Ray(transform.position, transform.forward);               // Raio criado a partir da posição da lanterna,
+                                                                                               // com origem na posição dela e sentido apontado pela lanterna (eixo Z azul).
+            
+            if (Physics.Raycast(ray, out RaycastHit hit, maxDistance))                         // RayCasting sem layer, apenas com (raio, hit info, maxDistance)
+            {                                                                                  // Se houver um Raycast com um HIT do ray a uma distância "maxDistance"...
+           
+                // verificar alternativas depois...
+            
+            /*
+             * if (hit.collider.gameObject.layer ==
+                   LayerMask.NameToLayer("Highlighted Objects"))
+               {
+                   Debug.Log(hit.collider.name);
+               }
+             */
+            
+            /*
+             * if ((myLayer.value & (1 << hit.collider.gameObject.layer)) != 0)
+             * 
+             * */
+             
+                if (myLayer.value == LayerMask.NameToLayer("Highlighted Objects"))
+                {
+                    Debug.Log(hit.collider.name);                                                  // ...nome do objeto é mostrado no console
+                }
+            }
+            
+            Debug.DrawRay(ray.origin, ray.direction * 5, Color.red);               // ray (em debug mode) acompanha o transform da lanterna (ver red line na aba Scene)
         }
-        
+
+       /* private void Update()
+        {
+            Debug.DrawRay(transform.position, transform.forward * 1000, Color.red);
+            
+            if (Physics.Raycast(transform.position, transform.forward, out RaycastHit hit, 1000, myLayer))      
+            {
+                Debug.Log("Hit: " + hit.collider.name);
+            };
+        }*/
     }
 }
