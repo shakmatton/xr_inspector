676b2f0465098e98f87af710a011298e4109e5b5
shakmatton <shakmatton@hotmail.com>
2026-08-20 12:58:56 -0300

    fixed checkbox change on event

--- Assets/Scripts/InspectionItem.cs
+++ Assets/Scripts/InspectionItem.cs
@@ -8,13 +8,15 @@ namespace Scripts
     public class InspectionItem : MonoBehaviour
     {
         [SerializeField] public TextMeshProUGUI textMeshProUGUI;                     // TextMeshProUGUI deverá ser arrastado aqui aparecer neste inspectionItem
-        [SerializeField] public Image image;                                         // imagem (checkbox) deverá aparecer neste inspectionItem
+        [SerializeField] public Sprite checkboxNo;                                   // imagem (sprite do checkbox não preenchido) que deverá aparecer neste inspectionItem
+        [SerializeField] public Sprite checkboxYes;                                  // imagem (sprite do checkbox já preenchido) que deverá eventualmente substituir a anterior (após eventos)
+
+        private Image _checkboxImage;                                                // imagem possui como "sub-atributo" um Sprite (acessar como "_checkboxImage.sprite")
         
         // Obs.: na hierarquia do editor Unity, deve-se fazer Image e Text serem filhos (separados) do gameObject InspectionItem (p/ ter maior controle do Transform de cada um). 
         
         [SerializeField] public Inspection inspection;                               // inspection deve ser arrastado aqui
                                                                                      // para cada prefab, algum inspection (de esfera, cubo, cilindro) deve ser arrastado nesse campo.
-        
         
         /* Passos:
          
@@ -25,12 +27,7 @@ namespace Scripts
             - configurar reação aos eventos do InspectionManager (tal como ocorrido com o ColorChanger).         */
         
         
-        
-        
             // continuar pensando como fazer as interações de eventos funcionarem para o gameObject... e no final, quando tudo tiver ok, criar 3 prefabs desse gameobject na cena......
-
-            
-            
             
             
         private void Start()                                                        // Ideia: já de início (no Start()), aparecer no painel o nome das tarefas (quests).
@@ -40,9 +37,26 @@ namespace Scripts
                 textMeshProUGUI.text = inspection.inspectionDescription;            // ...fazer texto do inspectionDescription aparecer aqui.
                                                                                     // Dica: p/ ver o nome das classes, clicar nos 3 pontinhos de cada componente (Image, TextMeshPro etc).
                                                                                     // Em TextMeshPro, ver que a classe herda de outra (o que permite usar diretamente a string "text")
+                                                                                    
+                _checkboxImage = GetComponentInChildren<Image>();                   // O filho do gameObject é do tipo Image. Salvamos ele em _checkboxImage.
+                _checkboxImage.sprite = checkboxNo;                                 // Sprite do _checkboxImage aponta para o sprite original ("checkbox_no")
             }
             
+            InspectionManager.Instance.OnSingleInspected += SingleQuestCompleted;   // evento do InspectionManager aponta para o callback SingleQuestCompleted.
+            // InspectionManager.Instance.OnFullInspected += AllQuestsCompleted;
+        }
+        
+
+        private void SingleQuestCompleted(Inspection inspection)
+        {
+            // if (inspection.inspectionName == inspectionRegion.inspectionData)    // ?????
+            
+            _checkboxImage.sprite = checkboxYes;                                    // sprite do checkbox atualizado para checkbox_yes 
         }
 
+        private void Update()
+        {
+            
+        }
     }
 }
