f65985ba400d0b999cf07fd39ab71328b95dbec4
shakmatton <shakmatton@hotmail.com>
2026-09-10 12:50:27 -0300

    retry fixing time inspection issue

--- Assets/Scripts/FlashlightCleanVersion.cs
+++ Assets/Scripts/FlashlightCleanVersion.cs
@@ -18,10 +18,10 @@ namespace Scripts
         private float maxDistance = 5f;                                        
         private bool flashlightSelected = false;
 
-        private float hoverTime;                                    // tempo de hover em um objeto
-        // private bool hoverFlag = false;                             // flag de controle sobre se o objeto sofre hover ou não
-        private string hoverObjectName;                             // flag de controle sobre qual é o nome do objeto corrente    
-        private string currentObj = "";                            // registra último objeto a sofrer hover
+        private float hoverTime;                                            // tempo de hover em um objeto
+        // private bool hoverFlag = false;                                  // flag de controle sobre se o objeto sofre hover ou não
+        private string hoverObjectName;                                     // flag de controle sobre qual é o nome do objeto corrente    
+        private string currentObj = "";                                     // ponteiro que registra último objeto a sofrer hover
 
         private void Start()                                                   
         {
@@ -56,63 +56,33 @@ namespace Scripts
                 // 3 - pensar no caso de 2 objetos grudados: como fica a questao do hover e do reset?
                 
                 
-                // VERIFICAR POR QUE NÃO FUNCIONA O CASO 3...
+                // PROBLEMA: AO SAIR E RETORNAR AO MESMO OBJETO, O TIMER NÃO RESETA! VERIFICAR (VER ARQUIVO teste.cs depois. na área de trabalho)...
                 
                 
-                
-                // TENTAR DE NOVO CONSERTAR O TRECHO ABAIXO (FAZER DEBUG.LOG() COM TEMPO DO OBJETO NO CONSOLE...)
-                
-                if (inspectionRegion != null)                                   // objeto inspecionado
+                if (inspectionRegion != null)                                                                // objeto inspecionado
                 {
-
-                    hoverObjectName = inspectionRegion.Inspection.inspectionName;
-
-                    if (currentObj != hoverObjectName)
-                    {
-                        // if (!hoverFlag)                                                
-                        // {
-                        //     hoverFlag = true;                                          
-                        hoverTime = inspectionRegion.Inspection.inspectionTime;
-                        currentObj = hoverObjectName;
-                        // }     
-                    }
+                    hoverObjectName = inspectionRegion.Inspection.inspectionName;                               // tento obter objeto e seu nome
                     
-                    // // no IF abaixo: primeiro frame detectado permite pegar o inspectionTime (duração de hover) do objeto
-                    // if (!hoverFlag)                                                // inicialmente falso
-                    // {
-                    //     hoverFlag = true;                                          // faz com que o if não seja mais executado no próximo ciclo de Update(). 
-                    //
-                    //     hoverTime = inspectionRegion.Inspection.inspectionTime;    // Pega o JSON inteiro (inspectionData) do inspectionRegion. O acesso ao inspectionTime se dá via struct.
-                    // }
+                    if (hoverObjectName != null){                                                               // se objeto é válido 
 
-                    // // Nesse if, asseguro a atualização do tempo de inspeção do objeto atual (deixa de ser o tempo antigo)
-                    // if (currentObj != hoverObjectName)                            // se o objeto não for mais o mesmo (ainda que em estado de hover ativo)... 
-                    // {
-                    //     hoverTime = inspectionRegion.Inspection.inspectionTime;    // ... atualizo e considero o tempo do novo objeto 
-                    // }
+                        if (currentObj != hoverObjectName){                                                     // se o ponteiro para o objeto difere do objeto válido
+                            hoverTime = inspectionRegion.Inspection.inspectionTime;                             // obtenho o tempo de inspeção do objeto
+                            currentObj = hoverObjectName;                                                       // atualizo o ponteiro de objeto para o objeto válido 
+                        }        
+        
+                        hoverTime -= Time.deltaTime;                                                            // tempo do objeto inspecionado sob hover é decrementado
+                        Debug.Log($"CurrentObjName = {currentObj} | HoverObjectName = {hoverObjectName}");      
+                        Debug.Log($"hoverTime = {hoverTime}");
 
-                    
-                    // if (currentObj != hoverObjectName)
-                    // {
-                    //     hoverFlag = false;
-                    //     currentObj = hoverObjectName;
-                    //     return;
-                    // }
-
-                    hoverTime -= Time.deltaTime;                                   // tempo do polígono sob hover é decrementado
-                    
-                    if (hoverTime <= 0)                                            // se o tempo do polígono esgotar...
-                    {
-                        InspectionManager.Instance.CheckInspection(inspectionRegion.Inspection);    // ... finalmente fazer a inspeção abaixo.
+                        if (hoverTime <= 0){                                                                    // tempo de inspeção concluído
+                            InspectionManager.Instance.CheckInspection(inspectionRegion.Inspection);            // chama o método de inspeção
+                        }
                     }
                 }
-                else    // inspectionRegion virou nulo, pois raio apontou para fora do objeto...
+                else                                                                                            // aqui, inspectionRegion é nulo, pois raio aponta p/ fora do objeto...
                 {
-                    // hoverFlag = false;     // se raio sair do objeto
-                    currentObj = "";
+                    currentObj = "";                                                                            // ... então, o ponteiro é resetado p/ nulo (pois agora aponta p/ "vazio")
                 }
-
-                // currentObj = hoverObjectName;
             }
             
             Debug.DrawRay(ray.origin, ray.direction * 5, Color.red);      
